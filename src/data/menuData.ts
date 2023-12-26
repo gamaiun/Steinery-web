1a42f42f867b4ec2746d1f39d5654b65e2f857d7
Vadim Antonovich <76610711+gamaiun@users.noreply.github.com>
2023-12-26 19:22:08 +0200

    New

--- src/data/menuData.ts
+++ src/data/menuData.ts
@@ -1,17 +1,4 @@
 export const menuData = [
-  // {
-  //   title: "About",
-  //   icon: "/images/icons/courses.svg",
-  //   link: "/about",
-  //   alt: "courses",
-  // },
-  // {
-  //   title: "Telegram",
-  //   icon: "/images/icons/telegram.png",
-  //   link: "/tutorials",
-  //   alt: "tutorials",
-  // },
-
   {
     title: "",
     icon: "/images/icons/hamburger.svg",
@@ -27,41 +14,18 @@ export const tooltipData = [
     link: "/profile",
     alt: "challenge",
   },
-  // {
-  //   title: "Plan",
-  //   icon: "/images/icons/cube.svg",
-  //   link: "/plans",
-  //   alt: "plans",
-  // },
-  // // { title: "Settings", icon: "/images/icons/settings.svg", link: "/settings" },
-  // {
-  //   title: "Sign out",
-  //   icon: "/images/icons/signout.svg",
-  //   link: "/logout",
-  //   alt: "logout",
-  // },
+
   {
     title: "About",
     icon: "/images/icons/cube.svg",
     link: "/about",
     alt: "courses",
   },
-  // {
-  //   title: "Telegram",
-  //   icon: "/images/icons/cube.svg",
-  //   link: "/tutorials",
-  //   alt: "tutorials",
-  // },
+
   {
     title: "Calendar",
     icon: "/images/icons/cube.svg",
     link: "/calendar",
     alt: "calendar",
   },
-  // {
-  //   title: "Weekly",
-  //   icon: "/images/icons/cube.svg",
-  //   link: "/weekly",
-  //   alt: "weekly",
-  // },
 ]
